995666b86b39b98f4c15ee46c9d58cc49d6f1fe2
Josh <jdpuglielli@gmail.com>
2022-02-21 14:05:28 -0500

    Created shopify scraper

--- sniffer.py
+++ sniffer.py
@@ -1,31 +1,37 @@
-import socket
-import struct
-import textwrap
+import requests
 
 
-# loop that runs as long as program runs, sit and wait for packet to come in, then extract information
-def main():
-    connection = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+class ShopifyScraper:
 
-    while True:
-        raw_data, address = connection.recvfrom(65536)
-        dest_mac, src_mac, eth_proto = ethernet_f(raw_data)  # take data pass to function, store in variables
-        print('\nEthernet Frame:')
-        print('Destination: {}, Source: {}, Protocol: {}'.format(dest_mac, src_mac, eth_proto))
+    def __init__(self, root_domain):
+        self.domain_url = root_domain
+        self.product_list_url = self.domain_url + "/products.json"
+        self.product_list = []
+
+    def get_products(self):
+
+        self.fetch_products = requests.get(self.product_list_url)
+
+        products = self.fetch_products.json()["products"]
+
+        for i in products:
+            title = i["title"]
+            slug = i["handle"]
+            publish_date = i["published_at"]
+            updated_date = i["updated_at"]
+            vendor = i["vendor"]
+            product_type = i["product_type"]
+            tags = i["tags"]
+            full_url = self.domain_url + "/products/" + slug
+
+            details = [title, full_url, publish_date, updated_date, vendor, product_type, tags]
+            self.product_list.append(details)
+
+    def print_products(self):
+        for product in self.product_list:
+            print(product)
 
 
-# unpack data, give information
-# gives destination, source, ethernet type, actual payload
-def ethernet_f(data):
-    dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[:14])  # read first 14 bytes of data
-    return get_mac_addr(dest_mac), get_mac_addr(src_mac), socket.htons(proto), data[14:]
-
-
-# mac address has format AB:CD:EF:GH:IJ:KL
-def get_mac_addr(bytes_address):
-    bytes_str = map('{:02x}'.format, bytes_address)     # format each chunk to two decimal places
-    mac_address = ':'.join(bytes_str).upper()   # take each of the two chunks, make sure that they're uppercase,
-    # join with colon
-    return mac_address
-
-main()
+x = ShopifyScraper("https://shop.protonmail.com/")
+x.get_products()
+x.print_products()

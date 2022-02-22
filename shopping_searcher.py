3e14ff95a5fe18f05b433b6b322c75a23a2ae93b
yix-shu <yix.shu723@gmail.com>
2022-02-22 13:47:09 -0800

    Using beautiful soup to find product matches

--- shopping_searcher.py
+++ shopping_searcher.py
@@ -10,4 +10,10 @@ driver = webdriver.Chrome()
 driver2 = webdriver.Chrome()
 driver.get("https://poshmark.ca/search?query=" + item + "&type=listings&src=dir")
 driver2.get("https://www.depop.com/search/?q=" + item)
-
+page = requests.get("https://poshmark.ca/search?query=" + item + "&type=listings&src=dir")
+page2 = requests.get("https://www.depop.com/search/?q=" + item)
+soup = bs4.BeautifulSoup(page.content, 'html.parser')
+soup2 = bs4.BeautifulSoup(page2.content, 'html.parser')
+#for i in soup.find_all('li', {'class': "styles__ProductCardContainer"}):
+match = soup2.find('li', class_="styles__ProductCardContainer-sc-__sc-13q41bc-8 cAJcNu")
+print(match)
